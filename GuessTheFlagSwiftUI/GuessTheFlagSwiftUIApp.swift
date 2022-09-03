429d74443042a01824ad247ad6f6d786979fead7
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-03 23:02:57 +0200

    Added initial project.

---
+++ GuessTheFlagSwiftUI/GuessTheFlagSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagSwiftUIApp.swift
+//  GuessTheFlagSwiftUI
+//
+//  Created by Anastasiia Veremiichyk on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
